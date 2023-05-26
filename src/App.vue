cfce7c96b3d92ef633d4da9792a9a6668dd90def
Khunn Satt <kset.dkk@gmail.com>
2023-05-26 17:21:53 +0630

    Lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Hello Vue JS</p>
+  <Modal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/ModalComponent.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+p {
+  color: blue;
 }
 </style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+        <p>Modal Component</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .backdrop{
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.5);
+    }
+    .modal{
+        width: 200px;
+        height: 100px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+        padding: 100px;
+        text-align: center;
+    }
+</style>
